d4acbb26c61f86196a8b475e5f9e7c54f19dfc48
Hyunjun Kim <njurn@njurn-mbp.local>
2015-07-18 23:42:52 +0900

    add express

--- server.js
+++ server.js
@@ -1,25 +1,6 @@
-var connect = require('connect');
-var app = connect();
-
-var logger = function(req, res, next) {
-	console.log(req.method, req.url);
-	next();
-};
-
-var helloWorld = function(req, res, next) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-};
-
-var goodbyeWorld = function(req, res, next) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Goodbye World');
-}
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
-
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var express = require('./config/express');
+var app = express();
 app.listen(3000);
+module.exports = app;
 console.log('Running...');
-
