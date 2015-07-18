09b170fa26a3e870f5f090c67d2e2ddc92bb44bb
Hyunjun Kim <njurn@njurn-mbp.local>
2015-07-19 02:22:50 +0900

    validation

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,60 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		index: true,
+		match: /.+\@.+\..+/
+	},
+	username: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		validate: [
+			function(password) {
+				return password.length >= 6;
+			},
+			'password should be longer'
+		]
+	},
+	role: {
+		type: String,
+		enum: ['Admin', 'Owner', 'User']
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
+
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+	var splitName = fullName.split(' ');
+	this.firstName = splitName[0] || '';
+	this.lastName = splitName[1] || '';
+});
+
+UserSchema.statics.findOneByUsername = function(username, callback) {
+	this.findOne({ username: new RegExp(username, 'i')}, callback);
+};
+
+UserSchema.methods.authenticate = function(password) {
+	return this.password === password;
+};
+
+UserSchema.post('save', function(next) {
+	if (this.isNew) {
+		console.log('A new user was created');
+	} else {
+		console.log('A user updated.');
+	}
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
